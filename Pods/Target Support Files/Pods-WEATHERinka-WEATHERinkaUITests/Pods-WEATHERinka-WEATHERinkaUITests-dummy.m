3c235c1a31dcf42687722eeca656ae42512cf924
Shabalin13 <d.shabal1n12@gmail.com>
2024-01-15 13:37:06 +0300

    WX-2 Подключение CocoaPods и добавление всех зависимостей

---
+++ Pods/Target Support Files/Pods-WEATHERinka-WEATHERinkaUITests/Pods-WEATHERinka-WEATHERinkaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WEATHERinka_WEATHERinkaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WEATHERinka_WEATHERinkaUITests
+@end
---
+++ Pods/Target Support Files/Pods-WEATHERinkaTests/Pods-WEATHERinkaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WEATHERinkaTests : NSObject
+@end
+@implementation PodsDummy_Pods_WEATHERinkaTests
+@end
