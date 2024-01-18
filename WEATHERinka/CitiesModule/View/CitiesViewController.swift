fd2c777e261b751316bc93235991d06ea98ced53
Shabalin13 <d.shabal1n12@gmail.com>
2024-01-18 13:17:36 +0300

    WX-5 Добавление Search Bar во View модуля Cities. Добавление дополнительного View для searching. Связывание View и ViewModel в модуле Cities для запроса, получения и отображения нужных городов с помощью search autocomplete

--- WEATHERinka/CitiesModule/View/CitiesViewController.swift
+++ WEATHERinka/CitiesModule/View/CitiesViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import RxSwift
 
 class CitiesViewController: UIViewController {
     
     private var viewModel: CitiesViewModelProtocol
+    private let disposeBag = DisposeBag()
+    
+    private lazy var searchController = UISearchController(searchResultsController: SearchResultsViewController())
     
     init(viewModel: CitiesViewModelProtocol) {
         self.viewModel = viewModel
@@ -23,11 +27,50 @@ class CitiesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(comeBackFromCities))
-        self.viewModel.getCitiesInfo(cityNameToAutocomplete: "Almet")
+        subscribe()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupView()
+    }
+    
+    private func subscribe() {
+        viewModel.cityItems.subscribe(onNext: { cityItems in
+            self.updateSearchResultsController(cityItems: cityItems)
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    private func setupView() {
+        self.view.backgroundColor = .systemBackground
+        
+        setupNavigationBar()
+        setupSearchController()
+    }
+    
+    private func setupNavigationBar() {
+        self.navigationItem.title = "Weather"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(comeBackFromCities))
+    }
+    
+    private func setupSearchController() {
+        self.navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for a city or airport"
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    private func updateSearchResultsController(cityItems: CityItems) {
+        if let searchResultsController = searchController.searchResultsController as? SearchResultsViewController {
+            searchResultsController.updateView(cityItems: cityItems)
+        }
+    }
+    
+    // MARK: - Routing
     @objc func comeBackFromCities() {
         viewModel.comeBackFromCities()
     }
@@ -36,4 +79,17 @@ class CitiesViewController: UIViewController {
         print("CitiesViewController deinit")
     }
 
+}
+
+extension CitiesViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let cityNameToAutocomplete = searchController.searchBar.text, !cityNameToAutocomplete.isEmpty {
+            print(cityNameToAutocomplete)
+            viewModel.getCitiesInfo(cityNameToAutocomplete: cityNameToAutocomplete)
+        } else if let searchResultsController = searchController.searchResultsController as? SearchResultsViewController {
+            searchResultsController.updateView(cityItems: [])
+        }
+    }
+    
 }
